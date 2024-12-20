5282b08d5ac1a7da826fdd48624a3caa26c368df
JADELKA / JDT <166043734+NioyJadelkaFp@users.noreply.github.com>
2024-12-20 17:52:18 -0300

    css
    json

--- routers/rutas.py
+++ routers/rutas.py
@@ -1,6 +1,12 @@
 from flask import render_template
+import json
 
-
-def Home():
-    TituloName:str = "Home"
+def Home(Ruta:str):
+    TituloName = Ruta
     return render_template('home.html', TituloName=TituloName)
+
+def Fnaf(Ruta:str):
+    TituloName = Ruta
+    with open('static/juegosFnaf.json', 'r') as file:
+        items = json.load(file)
+    return render_template('fnaf.html', TituloName=TituloName, items=items)
